9e208d4961854af222cb3e4e3203c1de13093db6
Odilkhon Mukhtorkhojaev <mmuhtor@gmail.com>
2019-07-02 01:22:33 +0500

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+      if (matrix[i][j] == '0') {
+        for (let k = 1; k <= 9; k++) {
+          if (isValid(matrix, i, j, k)) {
+            matrix[i][j] = k;
+          if (solveSudoku(matrix)) {
+           return matrix;
+          } else {
+           matrix[i][j] = '0';
+          }
+         }
+       }
+       return false;
+     }
+   }
+ }
+ return matrix;
+}
+function isValid(board, row, col, k) {
+    for (let i = 0; i < 9; i++) {
+        const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+        const n = 3 * Math.floor(col / 3) + i % 3;
+        if (board[row][i] == k || board[i][col] == k || board[m][n] == k) {
+          return false;
+        }
+    }
+    return true;
 }
